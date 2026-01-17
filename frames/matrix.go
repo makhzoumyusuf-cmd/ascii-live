44e85b17b9e837ffc30ebb7d95588f4021753afc
Yusufire <makhzoumyusuf@gmail.com>
2026-01-17 15:01:40 +0000

    Matrix Rain!

--- frames/matrix.go
+++ frames/matrix.go
@@ -6,56 +6,54 @@ import (
 	"time"
 )
 
-// DynamicMatrix generates a continuous Matrix rain
-var Matrix = DefaultFrameType(func() []string {
-	return []string{generateFrame()}
-})
+var Matrix = DefaultFrameType(generateMatrixFrames())
 
 var ROWS, COLS = 40, 120
-var drops []int
 
-func init() {
+func generateMatrixFrames() []string {
 	rand.Seed(time.Now().UnixNano())
-	drops = make([]int, COLS)
+	numFrames := 500 // lots of frames to avoid visible reset
+
+	drops := make([]int, COLS)
 	for i := 0; i < COLS; i++ {
 		drops[i] = rand.Intn(ROWS)
 	}
-}
 
-// generateFrame returns a single dynamic frame
-func generateFrame() string {
-	lines := make([]string, ROWS)
-	for r := 0; r < ROWS; r++ {
-		line := ""
-		for c := 0; c < COLS; c++ {
-			trailLength := 6
-			if r >= drops[c]-trailLength && r <= drops[c] {
-				line += string(randomChar())
-			} else {
-				line += " "
+	frames := make([]string, numFrames)
+
+	for f := 0; f < numFrames; f++ {
+		lines := make([]string, ROWS)
+		for r := 0; r < ROWS; r++ {
+			line := ""
+			for c := 0; c < COLS; c++ {
+				trailLength := 6
+				if r >= drops[c]-trailLength && r <= drops[c] {
+					line += string(randomChar())
+				} else {
+					line += " "
+				}
 			}
+			lines[r] = line
 		}
-		lines[r] = line
-	}
+		frames[f] = strings.Join(lines, "\n")
 
-	// Move drops down
-	for c := 0; c < COLS; c++ {
-		if rand.Float32() > 0.02 {
-			drops[c]++
-			if drops[c] >= ROWS {
+		// Move drops down
+		for c := 0; c < COLS; c++ {
+			if rand.Float32() > 0.02 {
+				drops[c]++
+				if drops[c] >= ROWS {
+					drops[c] = 0
+				}
+			} else {
 				drops[c] = 0
 			}
-		} else {
-			drops[c] = 0
 		}
 	}
 
-	return strings.Join(lines, "\n")
+	return frames
 }
 
-// randomChar returns a single Matrix character
 func randomChar() rune {
 	chars := "01ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@#$%^&*"
 	return rune(chars[rand.Intn(len(chars))])
 }
-
